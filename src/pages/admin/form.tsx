ebf305df8fac5013f33f3e03088a79e3a059f21c
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-10-15 13:12:05 -0700

    Updated form, added Dropzone

--- src/pages/admin/form.tsx
+++ src/pages/admin/form.tsx
@@ -1,10 +1,17 @@
-import { Group, Textarea, TextInput } from '@mantine/core';
-import { DatePicker } from '@mantine/dates';
+import { Group, Textarea, TextInput, useMantineTheme, Text } from '@mantine/core';
+import { DatePicker, TimeInput } from '@mantine/dates';
+import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from '@mantine/dropzone';
+import { IconClock, IconPhoto, IconUpload, IconX } from '@tabler/icons';
 import React, { useState } from 'react';
 
-const FormView = () => {
+const FormView = (props: Partial<DropzoneProps>) => {
+    const theme = useMantineTheme();
     return (
         <>
+        {/* Nombre de la ventana */}
+        <Group position='center'>
+            <h1>Crea un nuevo evento!</h1>
+        </Group>
         {/* Nombre del evento */}
             <TextInput
             placeholder="e.g. CETYS Expoingeniería"
@@ -17,10 +24,59 @@ const FormView = () => {
             label="Descripción"
             withAsterisk
             />
+        {/* Lugar */}
+            <TextInput
+            placeholder="e.g. CETYS Universidad Campus Ensenada"
+            label="Lugar"
+            withAsterisk
+            />
         {/* Date and time */}
             <Group>
-            <DatePicker placeholder="Selecciona tu fecha" label="Fecha" withAsterisk />
+                <DatePicker placeholder="Selecciona tu fecha" label="Fecha" withAsterisk />
+                <TimeInput
+                label="Pick time"
+                placeholder="Pick time"
+                icon={<IconClock size={16} />}
+                defaultValue={new Date()}
+                />
             </Group>
+        {/* Subir imagen */}
+
+            <Dropzone
+            onDrop={(files) => console.log('accepted files', files)}
+            onReject={(files) => console.log('rejected files', files)}
+            maxSize={3 * 1024 ** 2}
+            accept={IMAGE_MIME_TYPE}
+            {...props}
+            >
+                <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
+                    <Dropzone.Accept>
+                    <IconUpload
+                        size={50}
+                        stroke={1.5}
+                    />
+                    </Dropzone.Accept>
+                    <Dropzone.Reject>
+                    <IconX
+                        size={50}
+                        stroke={1.5}
+                        color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
+                    />
+                    </Dropzone.Reject>
+                    <Dropzone.Idle>
+                    <IconPhoto size={50} stroke={1.5} />
+                    </Dropzone.Idle>
+
+                    <div>
+                    <Text size="xl" inline>
+                        Drag images here or click to select files
+                    </Text>
+                    <Text size="sm" color="dimmed" inline mt={7}>
+                        Attach as many files as you like, each file should not exceed 5mb
+                    </Text>
+                    </div>
+                </Group>
+            </Dropzone>
         </>
     )
     
