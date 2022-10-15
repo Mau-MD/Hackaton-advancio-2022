e5d2637bd1f403b32794f87a2161188d19743dd8
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-10-15 15:51:56 -0700

    sms functions now receive all parameters

--- src/server/twilio/whatsapp.ts
+++ src/server/twilio/whatsapp.ts
@@ -1,10 +1,10 @@
 import client from './twilio';
 
-const sendWhatsapp = (number: string) => {
+const sendWhatsapp = (event_data: string, cellphone_number: string) => {
   var message = client.messages.create({
-    body: 'This is the whatsapp that made the Kessel Run in fourteen parsecs?',
+    body: "Don't forget your next appointment! " + event_data,
     from: 'whatsapp:+14155238886',
-    to: 'whatsapp:' + number
+    to: 'whatsapp:' + cellphone_number
     })
     .then((message: { status: any; }) =>  console.log(message.status))
     .done();
