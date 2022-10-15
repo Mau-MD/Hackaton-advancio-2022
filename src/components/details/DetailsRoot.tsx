bd5de74b55925a53b712d67e084ba28dd853d226
Daniel <danybarociov@gmail.com>
2022-10-15 11:22:04 -0700

    descripcion del evento

--- src/components/details/DetailsRoot.tsx
+++ src/components/details/DetailsRoot.tsx
@@ -1,5 +1,6 @@
 import { Box, Title, Text, Group, Stack, BackgroundImage } from "@mantine/core";
-``;
+import { useForm } from "@mantine/form";
+import ImageBanner from "./ImageBanner";
 const DetailsRoot = () => {
   return (
     <Box>
@@ -13,15 +14,26 @@ const DetailsRoot = () => {
           lugar del evento
         </Text>
       </Group>
-      <BackgroundImage
-        src={
-          "https://th.bing.com/th/id/R.07073e0950875368777a33e63b1c8a44?rik=TdtVWtZSse%2b0Kg&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f1%2fd%2f2%2f1523509-vertical-hd-wallpaper-1440x900-1920x1080-for-ios.jpg&ehk=0yj3mC%2f1yxRohKJnNnOvalCnm9nkdSpC4Hb8nHkqVso%3d&risl=&pid=ImgRaw&r=0"
-        }
-        radius="lg"
-        p={5}
-        mt={10}
-        sx={{ width: "100%", height: "350px" }}
-      ></BackgroundImage>
+      <ImageBanner />
+      <Stack mt={20}>
+        <Text size={"xl"} weight={700} color={"blue"}>
+          Descripcion
+        </Text>
+        <Text align={"justify"}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius aut
+          dicta voluptas neque mollitia repellendus voluptatum atque maxime
+          laudantium. Esse modi impedit rem. Similique possimus assumenda,
+          obcaecati cum consectetur nulla magni! Ducimus ea, consequatur beatae
+          necessitatibus commodi nam ullam ipsa recusandae nihil. Facere illum,
+          delectus facilis sint magnam, doloremque quisquam animi corporis
+          deserunt fuga sed exercitationem, consectetur tempore consequuntur.
+          Optio accusantium explicabo quasi nostrum fugiat numquam animi natus?
+          Ut in maxime eum, rerum quidem, commodi illum cumque velit facere
+          doloribus quae nobis voluptate. Commodi excepturi quam dicta obcaecati
+          quae itaque doloribus quos quis at! Voluptatibus veniam alias rerum
+          quia neque.
+        </Text>
+      </Stack>
     </Box>
   );
 };
