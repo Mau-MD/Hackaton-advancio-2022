867b5f2d47804da716c685ac8b5b587b0a8d2f65
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-10-15 17:14:41 -0700

    Dark Theme finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,11 @@ import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 import { MantineProvider } from "@mantine/styles";
-import { AppShell, Container, Text } from "@mantine/core";
+import { AppShell, ColorScheme, ColorSchemeProvider, Container, Text } from "@mantine/core";
 import { useState } from "react";
 import { HeaderSearchProps, Navbar } from "../components/core/Navbar";
 import { NotificationsProvider } from "@mantine/notifications";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
 
 interface NavLink {
   link?: string;
@@ -33,18 +34,31 @@ const MyApp: AppType<{ session: Session | null }> = ({
     { label: "Escuelas", links: schools || [] },
   ];
 
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
   return (
-    <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
-      <NotificationsProvider>
-        <SessionProvider session={session}>
-          <AppShell padding={"md"} header={<Navbar links={links} />}>
-            <Container>
-              <Component {...pageProps} />
-            </Container>
-          </AppShell>
-        </SessionProvider>
-      </NotificationsProvider>
-    </MantineProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider>
+          <SessionProvider session={session}>
+            <AppShell padding={"md"} header={<Navbar links={links} />}>
+              <Container>
+                <Component {...pageProps} />
+              </Container>
+            </AppShell>
+          </SessionProvider>
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 };
 
