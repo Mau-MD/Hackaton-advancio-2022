e8cbafbb99820890474eca1b4ed851cb84c886e3
Mauricio Munoz <j.mauricio.munoz1@gmail.com>
2022-10-15 17:53:33 -0700

    stuffv3

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,8 +1,8 @@
 import { router, publicProcedure } from "../trpc";
 import { z } from "zod";
-import sendSMS from '../../twilio/sms';
+import sendSMS from "../../twilio/sms";
 import sendWhatsapp from "../../twilio/whatsapp";
-import sendEmail from "../../twilio/email"
+import sendEmail from "../../twilio/email";
 
 export const exampleRouter = router({
   hello: publicProcedure
@@ -12,46 +12,52 @@ export const exampleRouter = router({
         greeting: `Hello ${input?.text ?? "world"}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-  sendSMS: publicProcedure.input(z.object({
-    event: z.object({
-      name_event: z.string(),
-      description_event: z.string(),
-      date_event: z.date(),
-      city_event: z.string(),
-      school_event: z.string()
-    }),
-    cellphone_number: z.string()
-  }))
+  sendSMS: publicProcedure
+    .input(
+      z.object({
+        event: z.object({
+          name_event: z.string(),
+          description_event: z.string(),
+          date_event: z.date(),
+          city_event: z.string(),
+          school_event: z.string(),
+        }),
+        cellphone_number: z.string(),
+      })
+    )
     .mutation(({ input }) => {
       sendSMS(input.event, input.cellphone_number);
-  }),
-  sendWhatsapp: publicProcedure.input(z.object({
-    event: z.object({
-      name_event: z.string(),
-      description_event: z.string(),
-      date_event: z.date(),
-      city_event: z.string(),
-      school_event: z.string()
     }),
-    cellphone_number: z.string()
-  }))
+  sendWhatsapp: publicProcedure
+    .input(
+      z.object({
+        event: z.object({
+          name_event: z.string(),
+          description_event: z.string(),
+          date_event: z.date(),
+          city_event: z.string(),
+          school_event: z.string(),
+        }),
+        cellphone_number: z.string(),
+      })
+    )
     .mutation(({ input }) => {
       sendSMS(input.event, input.cellphone_number);
-  }),
-  sendEmail: publicProcedure.input(z.object({
-    event: z.object({
-      name_event: z.string(),
-      description_event: z.string(),
-      date_event: z.date(),
-      city_event: z.string(),
-      school_event: z.string()
     }),
-    to_email: z.string()
-  }))
+  sendEmail: publicProcedure
+    .input(
+      z.object({
+        event: z.object({
+          name_event: z.string(),
+          description_event: z.string(),
+          date_event: z.date(),
+          city_event: z.string(),
+          school_event: z.string(),
+        }),
+        to_email: z.string(),
+      })
+    )
     .mutation(({ input }) => {
       sendEmail(input.event, input.to_email);
-  }),
+    }),
 });
